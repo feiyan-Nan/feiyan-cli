b208b4c07f3ba798358eba2c2c3f14ba43686d52
nanfeiyan <3328921305@qq.com>
2021-12-23 14:10:36 +0800

    devlop-log

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-const program = new Command();
-program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+// import { Command } from 'commander';
+//
+// const program = new Command();
+// program
+//   .option('-d, --debug', 'output extra debugging')
+//   .option('-s, --small', 'small pizza size')
+//   .option('-p, --pizza-type <type>', 'flavour of pizza');
+//
+// registerCommand();
+// // printLogo();
+//
+// function registerCommand() {
+// }
 
-console.log(process.argv, '123');
-
-program.parse(process.argv);
-
-const options = program.opts();
-if (options.debug) console.log(options, 'LLLL');
-console.log('pizza details:');
-if (options.small) console.log('- small pizza size');
-if (options.pizzaType) console.log(`- ${options.pizzaType}`);
+console.log('Hello world!');
