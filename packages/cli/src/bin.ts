691a5fbf38e8c5aed1b986314c16e99a362be275
nanfeiyan <3328921305@qq.com>
2021-12-23 16:38:12 +0800

    devlop

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+// import { Command } from 'commander';
 import { printLogo } from './utils';
 
-const program = new Command();
-program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+// const program = new Command();
+// program
+//   .option('-d, --debug', 'output extra debugging')
+//   .option('-s, --small', 'small pizza size')
+//   .option('-p, --pizza-type <type>', 'flavour of pizza');
 
 registerCommand();
 printLogo();
